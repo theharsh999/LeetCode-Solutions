b05579672fa97a605e4b905a98fad19f156c6e27
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-06-30 22:38:10 +0530

    Time: 1 ms (99.92%), Space: 46.6 MB (86.76%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,35 +10,46 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode merged = null;
-        for(int i=0; i<lists.length; i++){
-            merged = merge(merged, lists[i]);
+        if(lists.length == 0){
+            return null;
         }
-        return merged;
+        return mergeK(lists, 0, lists.length - 1);
     }
 
-    public ListNode merge(ListNode head1, ListNode head2){
+    public ListNode mergeK(ListNode[] lists, int st, int end) {
+        if (st == end) {
+            return lists[st];
+        }
+        int mid = (st + end) / 2;
+
+        ListNode leftMerged = mergeK(lists, st, mid);
+        ListNode rightMerged = mergeK(lists, mid+1, end);
+
+        return merge(leftMerged, rightMerged);
+    }
+
+    public ListNode merge(ListNode head1, ListNode head2) {
         ListNode mergedLL = new ListNode(-1);
         ListNode temp = mergedLL;
 
-        while(head1 != null && head2 != null){
-            if(head1.val < head2.val){
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
                 temp.next = head1;
                 head1 = head1.next;
-            }else{
+            } else {
                 temp.next = head2;
                 head2 = head2.next;
             }
             temp = temp.next;
         }
 
-        while(head1 != null){
+        while (head1 != null) {
             temp.next = head1;
             head1 = head1.next;
             temp = temp.next;
         }
 
-        while(head2 != null){
+        while (head2 != null) {
             temp.next = head2;
             head2 = head2.next;
             temp = temp.next;
