fdf1cf57b27e54173f19a17daae2ee0f2b9a661a
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-07-03 08:31:51 +0530

    Time: 3 ms (87.34%), Space: 43.4 MB (26.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() == 1)
+            return false;
+        Stack<Character> stk = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stk.push(ch);
+            } else if (!stk.isEmpty() && ((stk.peek() == '(' && ch == ')') || (stk.peek() == '[' && ch == ']')
+                    || (stk.peek() == '{' && ch == '}'))) {
+                stk.pop();
+            }else{
+                return false;
+            }
+        }
+        return stk.isEmpty();
+    }
+}
