289ac82c62cfbe2194181a39c9935c40cf9917a2
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-07-06 21:17:20 +0530

    Time: 57 ms (83.59%), Space: 76.3 MB (88.73%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,47 +1,26 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int nsl[] = new int[n];
-        int nsr[] = new int[n];
         Stack<Integer> s = new Stack<>();
-
-        // Next Smaller Left
-        for(int i=0; i<n; i++){
-            while(!s.isEmpty() && heights[i] <= heights[s.peek()]){
-                s.pop();
-            }
-            if(s.isEmpty()){
-                nsl[i] = -1;
-            }else{
-                nsl[i] = s.peek();
-            }
-            s.push(i);
-        }
-
-        // CLear the stack
-        s = new Stack<>();
-
-        // Next Smaller Right
-        for(int i=n-1; i>=0; i--){
-            while(!s.isEmpty() && heights[i] <= heights[s.peek()]){
-                s.pop();
-            }
-            if(s.isEmpty()){
-                nsr[i] = n;
-            }else{
-                nsr[i] = s.peek();
-            }
-            s.push(i);
-        }
-
-        // Calculating maxArea
         int maxArea = 0;
-        for(int i=0; i<n; i++){
-            int width = nsr[i] - nsl[i] - 1;
-            int currArea = heights[i] * width;
-            maxArea = Math.max(maxArea, currArea);
-        }
 
+        for (int i = 0; i < n; i++) {
+            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+                int idx = s.pop();
+                int height = heights[idx];
+                int left = s.isEmpty() ? -1 : s.peek();
+                int width = i - left - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            s.push(i);
+        }
+        while (!s.isEmpty()) {
+            int idx = s.pop();
+            int height = heights[idx];
+            int left = s.isEmpty() ? -1 : s.peek();
+            int width = n - left - 1;
+            maxArea = Math.max(maxArea, height * width);
+        }
         return maxArea;
     }
 }
