edf02a4f9df991651501e393fcf4aac345734275
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-07-12 20:58:08 +0530

    Time: 32 ms (51.36%), Space: 70.9 MB (92.22%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        if (arr.length == 0)
+            return arr;
+        int[] copy = arr.clone();
+        Arrays.sort(copy);
+
+        // remove duplicate from copied arr for accurate ranking
+        int j = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (copy[i] != copy[j]) {
+                copy[++j] = copy[i];
+            }
+        }
+
+        int ans[] = new int[arr.length];
+
+        // rank the elements in arr
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = binarySearch(copy, arr[i], j) + 1;
+        }
+
+        return ans;
+    }
+
+    private int binarySearch(int[] arr, int target, int last) {
+        int left = 0, right = last;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+}
