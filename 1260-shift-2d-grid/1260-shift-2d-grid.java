b43cfe770a14783f02bb34070820cb3305ca5e07
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-07-20 21:37:14 +0530

    Time: 6 ms (55.4%), Space: 47 MB (79.54%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -4,38 +4,30 @@ class Solution {
         int n = grid[0].length;
         int total = m * n;
         k %= total;
-
-        int arr[] = new int[total];
-        int x = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[x++] = grid[i][j];
-            }
-        }
         if (k != 0) {
-            reverse(arr, 0, total - 1);
-            reverse(arr, 0, k - 1);
-            reverse(arr, k, total - 1);
+            reverse(grid, n, 0, total - 1);
+            reverse(grid, n, 0, k - 1);
+            reverse(grid, n, k, total - 1);
         }
 
         List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             List<Integer> temp = new ArrayList<>();
             for (int j = 0; j < n; j++) {
-                temp.add(arr[i * n + j]);
+                temp.add(grid[i][j]);
             }
             ans.add(temp);
         }
         return ans;
     }
 
-    public void reverse(int arr[], int st, int end) {
-        while (st < end) {
-            int temp = arr[st];
-            arr[st] = arr[end];
-            arr[end] = temp;
-            st++;
-            end--;
+    public void reverse(int arr[][], int col, int i, int j) {
+        while (i < j) {
+            int temp = arr[i/col][j%col];
+            arr[i/col][j%col] = arr[j/col][i%col];
+            arr[j/col][i%col] = temp;
+            i++;
+            j--;
         }
     }
 }
