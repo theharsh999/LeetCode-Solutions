bb3a0c6015e19744130bbc84acc7c63a6efc2321
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-07-20 21:27:43 +0530

    Time: 5 ms (83.45%), Space: 47 MB (92.18%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int total = m * n;
+        k %= total;
+
+        int arr[] = new int[total];
+        int x = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[x++] = grid[i][j];
+            }
+        }
+        reverse(arr, 0, total - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, total - 1);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                temp.add(arr[i * n + j]);
+            }
+            ans.add(temp);
+        }
+        return ans;
+    }
+
+    public void reverse(int arr[], int st, int end) {
+        while (st < end) {
+            int temp = arr[st];
+            arr[st] = arr[end];
+            arr[end] = temp;
+            st++;
+            end--;
+        }
+    }
+}
