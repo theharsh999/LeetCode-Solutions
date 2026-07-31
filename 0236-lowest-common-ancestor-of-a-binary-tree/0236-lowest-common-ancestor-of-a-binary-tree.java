db0fa36fdfb77b98908436d2b978914bd5741167
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-07-31 21:47:32 +0530

    Time: 17 ms (21.81%), Space: 69.9 MB (55.92%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,39 +9,18 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> path1 = new ArrayList<>();
-        ArrayList<TreeNode> path2 = new ArrayList<>();
-
-        getPath(root, p.val, path1);
-        getPath(root, q.val, path2);
-
-        int i=0;
-        for(; i<path1.size() && i<path2.size(); i++){
-            if (path1.get(i) != path2.get(i)) {
-                break;
-            }
+        if(root == null || root == p || root == q){
+            return root;
         }
+        TreeNode leftLCA = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightLCA = lowestCommonAncestor(root.right, p, q);
 
-        TreeNode lca = path1.get(i-1);
-        return lca;
-    }
-
-    public boolean getPath(TreeNode root, int n, ArrayList<TreeNode> path){
-        if(root == null){
-            return false;
+        if(leftLCA == null){
+            return rightLCA;
         }
-        path.add(root);
-        if(root.val == n){
-            return true;
+        if(rightLCA == null){
+            return leftLCA;
         }
-
-        boolean foundLeft = getPath(root.left, n, path);
-        boolean foundRight = getPath(root.right, n, path);
-
-        if (foundLeft || foundRight) {
-            return true;
-        }
-        path.remove(path.size()-1);
-        return false;
+        return root;
     }
 }
