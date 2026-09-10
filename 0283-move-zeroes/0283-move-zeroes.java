c06137eedace2f6306c33de9e0c4205d7616bb19
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-09-10 22:00:47 +0530

    Time: 2 ms (91.79%), Space: 47.9 MB (20.18%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,11 +3,10 @@ class Solution {
         int j = 0;
         for(int i=0; i<nums.length; i++){
             if(nums[i] != 0){
-                nums[j++] = nums[i];
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = temp;
             }
-        }
-        while(j < nums.length){
-            nums[j++] = 0;
         }
     }
 }
