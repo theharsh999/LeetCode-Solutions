034ddb9c3519c65967aa23bd313a608d40a50469
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-09-15 19:17:18 +0530

    Time: 2 ms (96.24%), Space: 46.5 MB (98.03%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.java
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -1,10 +1,17 @@
 class Solution {
     public int[] numberGame(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length; i+=2){
-            int temp = nums[i];
-            nums[i] = nums[i+1];
-            nums[i+1] = temp;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<nums.length; i++){
+            pq.add(nums[i]);
+        }
+        int idx = 0;
+
+        while(!pq.isEmpty()){
+            int alice = pq.remove();
+            int bob = pq.remove();
+
+            nums[idx++] = bob;
+            nums[idx++] = alice;
         }
         return nums;
     }
