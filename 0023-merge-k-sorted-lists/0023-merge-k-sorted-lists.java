af6d3bfd166785da0729919dca4e712e9336f49d
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-09-16 12:47:15 +0530

    Time: 5 ms (40.72%), Space: 46.9 MB (46.57%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,50 +10,25 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0){
-            return null;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(
+                (a, b) -> Integer.compare(a.val, b.val));
+
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null) {
+                pq.add(lists[i]);
+            }
         }
-        return mergeK(lists, 0, lists.length - 1);
-    }
 
-    public ListNode mergeK(ListNode[] lists, int st, int end) {
-        if (st == end) {
-            return lists[st];
-        }
-        int mid = (st + end) / 2;
-
-        ListNode leftMerged = mergeK(lists, st, mid);
-        ListNode rightMerged = mergeK(lists, mid+1, end);
-
-        return merge(leftMerged, rightMerged);
-    }
-
-    public ListNode merge(ListNode head1, ListNode head2) {
-        ListNode mergedLL = new ListNode(-1);
-        ListNode temp = mergedLL;
-
-        while (head1 != null && head2 != null) {
-            if (head1.val < head2.val) {
-                temp.next = head1;
-                head1 = head1.next;
-            } else {
-                temp.next = head2;
-                head2 = head2.next;
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        while (!pq.isEmpty()) {
+            ListNode curr = pq.remove();
+            temp.next = curr;
+            if (curr.next != null) {
+                pq.add(curr.next);
             }
             temp = temp.next;
         }
-
-        while (head1 != null) {
-            temp.next = head1;
-            head1 = head1.next;
-            temp = temp.next;
-        }
-
-        while (head2 != null) {
-            temp.next = head2;
-            head2 = head2.next;
-            temp = temp.next;
-        }
-        return mergedLL.next;
+        return dummy.next;
     }
 }
