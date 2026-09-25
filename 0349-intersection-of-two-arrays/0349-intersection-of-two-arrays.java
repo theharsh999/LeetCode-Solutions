452247f21d7724baf6cf9ac995a613c0a5a83cb3
Harsh Patel <128370215+theharsh999@users.noreply.github.com>
2026-09-25 22:36:36 +0530

    Time: 2 ms (97.79%), Space: 45 MB (55.91%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,20 +1,21 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> s1 = new HashSet<>();
-        HashSet<Integer> s2 = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> common = new HashSet<>();
 
         for(int i=0; i<nums1.length; i++){
-            s1.add(nums1[i]);
+            set.add(nums1[i]);
         }
         for(int i=0; i<nums2.length; i++){
-            s2.add(nums2[i]);
+            if(set.contains(nums2[i])){
+                common.add(nums2[i]);
+            }
         }
 
-        s1.retainAll(s2);
-        int ans[] = new int[s1.size()];
+        int ans[] = new int[common.size()];
         int i=0;
 
-        for(int x : s1){
+        for(int x : common){
             ans[i++] = x;
         }
         return ans;
